682ee142bf00d544b1da93168546708d3b71c2d8
Anthony <ajtalvarez@gmail.com>
2017-12-15 19:37:04 -0500

    Added model document middleware and custom instance method to users model.

--- models/users.js
+++ models/users.js
@@ -1,8 +1,25 @@
+// Dependencies
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
+// User Schema
 const userSchema = new mongoose.Schema({
   username: {type: String, required: true},
   password: {type: String, required: true}
 });
+
+// Model Document Middleware
+userSchema.pre('save', function(next) {
+  if (this.isModified('password')) {
+    const hashedPassword = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
+    this.password = hashedPassword;
+  }
+  next();
+});
+
+//  Custom Instance Method
+userSchema.methods.authenticate = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model("User", userSchema);
