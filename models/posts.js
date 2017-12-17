7491426c7ac6da6301789bbc7d3efbaf62bd72cb
Anthony <ajtalvarez@gmail.com>
2017-12-17 16:47:57 -0500

    Added comments to sessions and posts controllers for debugging. Split tags in posts controller. Linked User model and required user attribute in posts model.

--- models/posts.js
+++ models/posts.js
@@ -1,11 +1,13 @@
-const mongoose     = require('mongoose');
+const mongoose      = require('mongoose');
+const User          = require('../models/users.js');
 
 const postSchema   = mongoose.Schema({
   artist: { type: String, require: true },
   songTitle: { type: String, require: true },
   url: { type: String, require: true },
   tag: [{ type: String }],
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  // user: {type: String, required: true}
 });
 
 module.exports = mongoose.model('Post', postSchema);
