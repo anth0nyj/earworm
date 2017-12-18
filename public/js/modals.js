96ad1c22f35835952744dc602f6e938ea1e42db6
Foryst Jankelat commit --amend --reset-author <fjankela@outlook.com>
2017-12-18 13:49:26 -0800

    drop down menu for new post created as well as fonts adjusted

--- public/js/modals.js
+++ public/js/modals.js
@@ -5,6 +5,8 @@ $( () => {
   const $registerModal = $('.register-modal');
   const $register = $('.register');
   const $closeModal = $('.close-modal');
+  const $postMenu = $('.post_menu');
+  const $postBar = $('.post_bar');
 
   const loginModal = () => {
     $loginModal.css('display', 'block');
@@ -13,12 +15,25 @@ $( () => {
     $registerModal.css('display', 'block');
   }
   const closeBox = () => {
-  $loginModal.css('display', 'none');
-  $registerModal.css('display', 'none');
-}
+    $loginModal.css('display', 'none');
+    $registerModal.css('display', 'none');
+  }
+  const openMenu = () => {
+    let clicks = $(this).data('clicks');
+    if (clicks) {
+    $postMenu.css('display', 'none');
+    } else {
+    $postMenu.css('display', 'flex');
+    }
+$(this).data("clicks", !clicks);
+  }
+  // const closeMeu = () => {
+  //   $postMenu.css('display: none');
+  // }
 
   $login.on('click', loginModal);
   $register.on('click', registerModal);
   $closeModal.on('click', closeBox);
+  $postBar.on('click', openMenu);
 
 });
